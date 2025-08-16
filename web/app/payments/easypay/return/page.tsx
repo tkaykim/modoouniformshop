b4725bcfb4ec6c1f5475701a91f64e823753075b
tkaykim <tommy0621@naver.com>
2025-08-16 21:04:20 +0900

    order

--- web/app/payments/easypay/return/page.tsx
+++ web/app/payments/easypay/return/page.tsx
@@ -1,7 +1,14 @@
+import { Suspense } from "react";
 import ReturnClient from "./ReturnClient";
 
+export const dynamic = 'force-dynamic';
+
 export default function EasypayReturnPage() {
-  return <ReturnClient />;
+  return (
+    <Suspense fallback={<div className="max-w-xl mx-auto px-4 py-12">결제 처리 중...</div>}>
+      <ReturnClient />
+    </Suspense>
+  );
 }
 
 
